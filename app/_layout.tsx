8b94ea29821fda31a86b3c89fdabde9f3761518a
tudor <tudor.ileni@tvarita.ro>
2024-11-11 10:01:21 +0200

    add bottom navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,42 @@
-import { Stack } from "expo-router";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Index from './index';
+import Profile from './profile';
+import Events from './events';
+// import Settings from './Settings';
+
+const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
+    <Tab.Navigator initialRouteName="index">
+      <Tab.Screen
         name="index"
+        component={Index}
         options={{
-          headerShown: false,
+          tabBarLabel: 'Home',
         }}
       />
-      <Stack.Screen
+      <Tab.Screen
         name="profile"
+        component={Profile}
         options={{
-          headerShown: false,
+          tabBarLabel: 'Profile',
         }}
       />
-    </Stack>
+      <Tab.Screen
+        name="events"
+        component={Events}
+        options={{
+          tabBarLabel: 'Events',
+        }}
+      />
+      {/* <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarLabel: 'Settings',
+        }}
+      /> */}
+    </Tab.Navigator>
   );
 }
