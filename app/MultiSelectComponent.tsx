93146383c1e9c68fd1bf463bbed869850d2bb16d
Cristian Tiut <tiutcristian@gmail.com>
2025-02-16 02:22:21 +0200

    basic UI done
    TODO: add role input for members of a project

---
+++ app/MultiSelectComponent.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MultiSelect } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+interface MultiSelectComponentProps {
+	data: { label: string; value: string }[];
+	placeholder: string;
+}
+
+function MultiSelectComponent({ data, placeholder }: MultiSelectComponentProps) {
+	const [selected, setSelected] = useState<string[]>([]);
+
+	const renderItem = (item: any) => {
+		return (
+			<View style={styles.item}>
+				<Text style={styles.selectedTextStyle}>{item.label}</Text>
+			</View>
+		);
+	};
+
+	const renderSelectedItem = (item: any, unSelect: any) => {
+		return (
+			<TouchableOpacity onPress={() => unSelect && unSelect(item)}>
+				<View style={styles.selectedStyle}>
+					<Text style={styles.textSelectedStyle}>{item.label}</Text>
+					<AntDesign name="close" size={16} color="black" />
+				</View>
+			</TouchableOpacity>
+		);
+	};
+
+	return (
+		<View style={styles.container}>
+			<MultiSelect
+				style={styles.dropdown}
+				selectedTextStyle={styles.selectedTextStyle}
+				inputSearchStyle={styles.inputSearchStyle}
+				iconStyle={styles.iconStyle}
+				backgroundColor={'rgba(0,0,0,0.2)'}
+				data={data}
+				labelField="label"
+				valueField="value"
+				placeholder={placeholder}
+				value={selected}
+				search
+				searchPlaceholder="Search..."
+				onChange={(item) => { setSelected(item); }}
+				renderItem={renderItem}
+				renderSelectedItem={renderSelectedItem}
+			/>
+		</View>
+	);
+};
+
+export default MultiSelectComponent;
+
+const styles = StyleSheet.create({
+	container: { marginBottom: 20 },
+	dropdown: {
+		width: "100%",
+		height: 40,
+		backgroundColor: 'white',
+		borderColor: "#ccc",
+		borderWidth: 1,
+		borderRadius: 5,
+		paddingHorizontal: 10,
+	},
+	icon: {
+		marginRight: 5,
+	},
+	selectedTextStyle: {
+		fontSize: 14,
+	},
+	iconStyle: {
+		width: 20,
+		height: 20,
+	},
+	inputSearchStyle: {
+		height: 40,
+		fontSize: 16,
+	},
+	item: {
+		padding: 17,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	selectedStyle: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 14,
+		backgroundColor: 'white',
+		shadowColor: '#000',
+		marginTop: 8,
+		marginLeft: 5,
+		marginRight: 5,
+		paddingHorizontal: 12,
+		paddingVertical: 8,
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 1.41,
+		elevation: 2,
+	},
+	textSelectedStyle: {
+		marginRight: 5,
+	},
+});
--- app/projects.tsx
+++ app/projects.tsx
@@ -1,124 +1,168 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Modal, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, Modal, TextInput, TouchableOpacity } from "react-native";
 import { FAB } from "react-native-elements";
-import DateTimePicker from '@react-native-community/datetimepicker';
+import MultiSelectComponent from "./MultiSelectComponent";
+import TagInputComponent from "./TagInputComponent";
+import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
+import DatePickerComponent from "./DatePickerComponent";
 
 const Projects = () => {
-  const [modalVisible, setModalVisible] = useState(false);
-  const [projectName, setProjectName] = useState("");
-  const [date, setDate] = useState(new Date());
+	const [modalVisible, setModalVisible] = useState(false);
+	const [projectName, setProjectName] = useState("");
 
-  const handleAddProject = () => {
-    setModalVisible(true);
-  };
+	const dataForMembersDropdown = [
+		{ label: 'Item 1', value: '1' },
+		{ label: 'Item 2', value: '2' },
+		{ label: 'Item 3', value: '3' },
+		{ label: 'Item 4', value: '4' },
+		{ label: 'Item 5', value: '5' },
+		{ label: 'Item 6', value: '6' },
+		{ label: 'Item 7', value: '7' },
+		{ label: 'Item 8', value: '8' },
+	];
 
-  const handleSaveProject = () => {
-    // Save project to database ...
-    setModalVisible(false);
-  };
+	const handleAddProject = () => {
+		setModalVisible(true);
+	};
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Projects Page</Text>
-      <FAB
-        icon={{ name: 'add', color: 'white' }}
-        placement="right"
-        color="#6200EE"
-        onPress={handleAddProject}
-      />
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={styles.modalView}>
-          <Text style={styles.modalText}>Add New Project</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Project Name"
-            value={projectName}
-            onChangeText={setProjectName}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Project Description"
-            numberOfLines={4}
-          />
-          {/* TODO Display date that when clicked opens the picker */}
-          <DateTimePicker
-            value={date}
-            mode="date"
-            display="default"
-            onChange={(event, selectedDate) => {
-              const currentDate = selectedDate || date;
-              setDate(currentDate);
-            }}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Keywords"
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Members"
-          />
-          <View style={styles.actionButtonsView}>
-            <Button title="Save" onPress={handleSaveProject} />
-            <Button title="Cancel" onPress={() => setModalVisible(false)} />
-          </View>
-        </View>
-      </Modal>
-    </View>
-  );
+	const handleSaveProject = () => {
+		// TODO Save project to database ...
+		setModalVisible(false);
+	};
+
+	return (
+		<GestureHandlerRootView style={styles.container}>
+			<Text style={styles.text}>Projects Page</Text>
+			<FAB
+				icon={{ name: 'add', color: 'white' }}
+				placement="right"
+				color="#6200EE"
+				onPress={handleAddProject}/>
+			<Modal
+				animationType="slide"
+				transparent={true}
+				visible={modalVisible}
+				onRequestClose={() => setModalVisible(false)}
+			>
+				<View style={styles.modalView}>
+					<ScrollView>
+						<Text style={styles.modalText}>Add New Project</Text>
+
+						<TextInput
+							style={styles.input}
+							placeholder="Project Name"
+							value={projectName}
+							onChangeText={setProjectName}
+						/>
+
+						<TextInput
+							style={styles.inputMultiline}
+							placeholder="Description"
+							multiline
+							numberOfLines={4}
+						/>
+						
+						<DatePickerComponent />
+
+						<TagInputComponent />
+
+						<MultiSelectComponent
+							data={dataForMembersDropdown}
+							placeholder="Select Members"
+						/>
+
+						<View style={styles.actionButtonsView}>
+							<TouchableOpacity 
+								onPress={handleSaveProject} 
+								style={styles.actionButtons}
+							>
+								<Text style={styles.buttonText}>Save</Text>
+							</TouchableOpacity>
+							
+							<TouchableOpacity 
+								onPress={() => setModalVisible(false)} 
+								style={styles.actionButtons}
+							>
+								<Text style={styles.buttonText}>Cancel</Text>
+							</TouchableOpacity>
+						</View>
+					</ScrollView>
+				</View>
+			</Modal>
+		</GestureHandlerRootView>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5",
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#333",
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#f5f5f5",
+	},
+	text: {
+		fontSize: 20,
+		fontWeight: "bold",
+		color: "#333",
+	},
+	modalView: {
+		margin: 20,
+		backgroundColor: "white",
+		borderRadius: 20,
+		padding: 35,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+	modalText: {
+		marginBottom: 15,
+		textAlign: "center",
+		fontSize: 18,
+		fontWeight: "bold",
+	},
+	input: {
+		height: 40,
+		borderColor: "#ccc",
+		borderWidth: 1,
+		borderRadius: 5,
+		marginBottom: 15,
+		paddingHorizontal: 10,
+		width: "100%",
+	},
+	inputMultiline: {
+		height: 80,
+		borderColor: "#ccc",
+		borderWidth: 1,
+		borderRadius: 5,
+		marginBottom: 15,
+		paddingHorizontal: 10,
+		paddingVertical: 10,
+		textAlignVertical: "top",
+		width: "100%",
+	},
+	actionButtonsView: {
+		flexDirection: "row",
+		justifyContent: "space-around",
+		width: "100%",
+	},
+	actionButtons: {
+        backgroundColor: '#6200EE',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 5,
+		width: "30%",
+		alignItems: "center",
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  input: {
-    height: 40,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    marginBottom: 15,
-    paddingHorizontal: 10,
-    width: "100%",
-  },
-  actionButtonsView: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "100%",
-  },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+	},
 });
 
 export default Projects;
